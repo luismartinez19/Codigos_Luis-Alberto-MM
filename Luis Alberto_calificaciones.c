85ca1dbfad287dea1dd746ae11e37a2d630c636e
luismartinez19 <71953771+luismartinez19@users.noreply.github.com>
2020-10-02 21:51:50 -0500

    Add files via upload

---
+++ Luis Alberto_calificaciones.c
@@ -0,0 +1,26 @@
+//programa de Luis Alberto Martinez Morales para calcular el promedio de 3 calificaciones.
+#include <stdio.h>
+#include <conio.h>
+
+main()
+{
+    float c1,c2,c3,resultado;
+    printf("cuál es tu primera calificacion?");
+    scanf("%f", &c1);
+    printf("cuál es tu segunda calificacion?");
+    scanf("%f", &c2);
+    printf("cuál es tu tercera calificacion?");
+    scanf("%f", &c3);
+    resultado=(c1+c2+c3)/3;
+
+    if(resultado<=8)
+    {
+        printf("reprobado");
+    }
+    else{
+        printf("aprobado");
+    }
+    printf("tu promedio es %f", resultado);
+
+    return 0;
+}
